234203096646367ca073879e5966a7dc2863462c
Alvaro Perez <alvpdiaz@gmail.com>
2016-08-26 16:03:14 +0100

    Configured 1102 for circleci

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -5,23 +5,23 @@ echo ----- STARTING INSTALLATION -----
 echo ---------------------------------
 
 # Download Forge 1.8 from the internet
-curl -o forge-18.zip http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8-11.14.4.1563/forge-1.8-11.14.4.1563-mdk.zip
+curl -o forge.zip http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.10.2-12.18.1.2011/forge-1.10.2-12.18.1.2011-mdk.zip
 
 # Create a directory to host Forge files and decompress the .zip file there
-unzip forge-18.zip -d forge-18
+unzip forge.zip -d forge
 
 # Setup workspace, may take a while
-cd forge-18
+cd forge
 ./gradlew setupDecompWorkspace --refresh-dependencies
 cd ..
 
 # Move the mod code files to the newly created Forge folder
-mv build.gradle forge-18/
-mv gradle forge-18/gradle/
-mv gradlew forge-18/
-mv gradlew.bat forge-18/
-rm -r forge-18/src
-mv src forge-18/
+mv build.gradle forge/
+mv gradle forge/gradle/
+mv gradlew forge/
+mv gradlew.bat forge/
+rm -r forge/src
+mv src forge/
 
 echo ---------------------------------
 echo ----- FINISHED INSTALLATION -----
