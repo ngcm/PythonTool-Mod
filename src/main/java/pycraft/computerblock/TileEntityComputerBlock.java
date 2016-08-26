9b2c93451c493ee67b783da0f1d6e4903343e3a9
Alvaro Perez <alvpdiaz@gmail.com>
2016-08-26 14:15:41 +0100

    Working mod 0.8 in minecraft1.8.9

--- src/main/java/pycraft/computerblock/TileEntityComputerBlock.java
+++ src/main/java/pycraft/computerblock/TileEntityComputerBlock.java
@@ -187,7 +187,7 @@ public class TileEntityComputerBlock extends TileEntity implements IInventory {
 	 * @return
 	 */
 	@Override
-	public ItemStack getStackInSlotOnClosing(int slotIndex) {
+	public ItemStack removeStackFromSlot(int slotIndex) {
 		ItemStack itemStack = getStackInSlot(slotIndex);
 		if (itemStack != null) setInventorySlotContents(slotIndex, null);
 		return itemStack;
