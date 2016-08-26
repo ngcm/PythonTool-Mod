0cc98e02525c32c50032dccb6ac6ef13525b0500
Alvaro Perez Diaz <alvpdiaz@gmail.com>
2016-08-26 11:11:34 +0100

    Working mod 0.8 in minecraft1.10.2

--- src/main/java/pycraft/computerblock/StartupClientOnly.java
+++ src/main/java/pycraft/computerblock/StartupClientOnly.java
@@ -1,9 +1,8 @@
 package pycraft.computerblock;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 
 /**
  * ----------- PyCraft Mod -----------
@@ -31,10 +30,6 @@ public class StartupClientOnly
 {
 	public static void preInitClientOnly()
 	{
-	}
-
-	public static void initClientOnly()
-	{
 		// This is currently necessary in order to make your block render properly when it is an item (i.e. in the inventory
 		//   or in your hand or thrown on the ground).
 		// Minecraft knows to look for the item model based on the GameRegistry.registerBlock.  However the registration of
@@ -42,10 +37,13 @@ public class StartupClientOnly
 		//   of any extra items you have created.  Hence you have to do it manually.  This will probably change in future.
 		// It must be done in the init phase, not preinit, and must be done on client only.
 
-		Item itemComputerBlock = GameRegistry.findItem("pycraft", "computerBlock");
 		ModelResourceLocation itemModelResourceLocation = new ModelResourceLocation("pycraft:computerBlock", "inventory");
 		final int DEFAULT_ITEM_SUBTYPE = 0;
-		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(itemComputerBlock, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+	    ModelLoader.setCustomModelResourceLocation(StartupCommon.itemComputerBlock, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+	}
+	
+	public static void initClientOnly()
+	{
 	}
 
 	public static void postInitClientOnly()
