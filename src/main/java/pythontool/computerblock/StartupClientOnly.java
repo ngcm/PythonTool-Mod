2f3593e22c55ffc1fc9e16ff059d28083f5f2e96
Alvaro Perez <alvpdiaz@gmail.com>
2016-09-08 11:45:57 +0100

    Changed mod name to PythonTool-Mod

---
+++ src/main/java/pythontool/computerblock/StartupClientOnly.java
@@ -0,0 +1,52 @@
+package pythontool.computerblock;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+
+/**
+ * ----------- PythonTool Mod -----------
+ * Alvaro Perez & Hans Fangohr
+ * University of Southampton, UK (2016)
+ *
+ * Code based on MinecraftByExample by TheGreyGhost: MBE30_INVENTORY_BASIC
+ * https://github.com/TheGreyGhost/MinecraftByExample/tree/1-8final
+ * (mbe30_inventory_basic/StartupClientOnly.java)
+ * 
+ * Original code by:
+ * User: brandon3055
+ * Date: 06/01/2015
+ *
+ * The Startup class for this example is called during startup, in the following order:
+ *  preInitCommon
+ *  preInitClientOnly
+ *  initCommon
+ *  initClientOnly
+ *  postInitCommon
+ *  postInitClientOnly
+ *  See MinecraftByExample class for more information
+ */
+public class StartupClientOnly
+{
+	public static void preInitClientOnly()
+	{
+		// This is currently necessary in order to make your block render properly when it is an item (i.e. in the inventory
+		//   or in your hand or thrown on the ground).
+		// Minecraft knows to look for the item model based on the GameRegistry.registerBlock.  However the registration of
+		//  the model for each item is normally done by RenderItem.registerItems(), and this is not currently aware
+		//   of any extra items you have created.  Hence you have to do it manually.  This will probably change in future.
+		// It must be done in the init phase, not preinit, and must be done on client only.
+
+		ModelResourceLocation itemModelResourceLocation = new ModelResourceLocation("pythontool:computerBlock", "inventory");
+		final int DEFAULT_ITEM_SUBTYPE = 0;
+	    ModelLoader.setCustomModelResourceLocation(StartupCommon.itemComputerBlock, DEFAULT_ITEM_SUBTYPE, itemModelResourceLocation);
+	}
+	
+	public static void initClientOnly()
+	{
+	}
+
+	public static void postInitClientOnly()
+	{
+	}
+}
