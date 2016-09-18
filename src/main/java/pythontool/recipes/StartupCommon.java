55d95829b3b84809287387eb9364c5815a0a480b
Alvaro Perez <alvpdiaz@gmail.com>
2016-09-18 10:58:37 +0100

    Added new recipe and website

--- src/main/java/pythontool/recipes/StartupCommon.java
+++ src/main/java/pythontool/recipes/StartupCommon.java
@@ -51,8 +51,8 @@ public class StartupCommon
 				"RRR",
 				"RGR",
 				"RRR",
-				'G', Blocks.glass_pane,
-				'R', Blocks.bedrock
+				'G', Blocks.glass,
+				'R', Blocks.cobblestone
 		});	
 	}
 
